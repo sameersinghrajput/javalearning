65f431b486632767e657fc8cdec7e358cac9dae1
Sameer Rajput <sameerrajput7834@gmail.com>
2022-12-02 10:25:13 +0530

    hello

---
+++ loopPatternQuestions1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class loopPatternQuestions1 {
+    public static void main(String[] args) {
+       
+        Scanner sc=new  Scanner(System.in);
+        System.out.println("enter rows and columns");
+        int row=sc.nextInt();
+        int i;
+       // int col=sc.nextInt();
+        for(i=0;i<row;i++){
+            System.out.print("*");
+        }
+      
+    }
+    
+}
